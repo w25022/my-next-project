4c833387eab79c9bffb0ca19c9943da4f758cdf9
LINNSATAUNG <w25022@osfl.ac.jp>
2026-06-09 15:10:43 +0900

    8章まで完了

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,11 @@
-// import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest){
-//     console.log("middleware: " + request.url);
+export function middleware(request: NextRequest) {
+  console.log("middleware:", request.url);
+  return NextResponse.next();
+}
 
-//     return NextResponse.next();
-// }
-
-// export const config ={
-
-// matcher : ["/about/:path*", "/dashboard/:path*"],
-// }
-
-import {createNextAuthMiddleware} from "nextjs-basic-auth-middleware";
-
-export const middleware = createNextAuthMiddleware();
 export const config = {
-    matcher: ["/(.*)"],
+  matcher: ["/(.*)"],
 };
